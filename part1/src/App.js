d997bebbcd9312efaa2b769b769ffd79b8fcf427
Nelson E Diaz P <13681608+neslondiaze@users.noreply.github.com>
2023-09-10 12:29:38 -0400

    First commit

---
+++ part1/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Mensaje from './mensaje/Mensaje';
+
+
+const  Description = () => {
+return <p>Esta es la app del curso fullstack bootcamp</p>
+}
+
+
+function App() {
+  return (
+    <div className="App">
+      <Mensaje color='red' message='Estamos trabajando'/>
+      <Mensaje color='green'  message='En un curso'/>
+      <Mensaje color='blue'  message='De React'/>
+      <Description/>
+    </div>
+  );
+}
+
+export default App;
