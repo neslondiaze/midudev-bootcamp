2e1ceac37bdc5d39396483249b94eeacf28e6f8e
Nelson E Diaz P <13681608+neslondiaze@users.noreply.github.com>
2023-09-10 12:52:45 -0400

    Frist commit part1-react

---
+++ part0/web-ejemplo/script.js
@@ -0,0 +1,27 @@
+const list = [];
+const anotherList = list.concat(157);
+
+const persona = {
+    name: 'Nelson',
+    twitter: '@nelsondiaze',
+    age: 55,
+    isDeveloper: true,
+    links: ['https://nelsondiaze.tube', 'https://nelsondiaze.live']
+}
+
+// Expresion
+const suma = (operando1, operando2) => {
+    console.log(operando1);
+    console.log(operando2);
+    return operando1 + operando2;
+}
+
+//Declaracion
+ function restar(a, b){
+    return a - b;
+ }
+
+const op1 = 8;
+const op2 = 2;
+
+console.log(suma(op1, op2))
