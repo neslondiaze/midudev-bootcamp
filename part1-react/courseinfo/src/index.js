28d4bd77f351a9cd6e61619a33124df73f6a72ee
Nelson E Diaz <neslondiaze@gmail.com>
2023-09-12 10:10:31 -0400

    commit the Debian

--- part1-react/courseinfo/src/index.js
+++ part1-react/courseinfo/src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Title = ({course}) => <h1>{course}</h1>
+const Parts = ( {part},{exercises}) => {
+  return(
+  <p>
+  {part} {exercises}
+  </p>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Title course={course} />
+      <Parts part={part1} exercises={exercises1} />
+      <Parts part={part2} exercises={exercises2} />
+      <Parts part={part3} exercises={exercises3} />
+
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
